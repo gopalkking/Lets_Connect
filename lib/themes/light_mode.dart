d402b959942b3479ad82b68a249c38c42eb62193
gopalkking <gopal392002@gmail.com>
2024-01-21 22:58:41 +0530

    theme modified

---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Color.fromARGB(255, 226, 167, 40),
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade200 ,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade900,
+  )
+);
