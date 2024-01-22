8230269261a4e8127ef04df850b51311df18272c
gopalkking <gopal392002@gmail.com>
2024-01-23 00:14:01 +0530

    login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,65 @@
 import "package:flutter/material.dart";
+import "package:lets_connect/components/my_button.dart";
+import "package:lets_connect/components/my_textfield.dart";
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _pwcontroller = TextEditingController();
+
+
+   LoginPage({super.key});
+
+ void login(){
+
+ }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          //logo
+          Icon(
+            Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          const SizedBox(height: 50,),
+          //welcome message
+          Text("Hey! The chat room lights up when you join",
+          style: TextStyle(color: Colors.black87,
+          fontSize: 20,
+          ),
+          ),
+          const SizedBox(height: 25),
+          //email textfield
+          MyTextField(
+            hintText: "Email",
+            obscureText: false,
+            controller: _emailcontroller,
+          ),
+         const SizedBox(height: 10,),
+          //pw  textfield
+         MyTextField(
+            hintText: "Password",
+            obscureText: true,
+            controller: _pwcontroller,
+          ),
+        const SizedBox(height: 25,),
+          //login button 
+          MyButton(
+            text: "Login",
+            onTap: login,
+          ),
+        
+          //register now
+        
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
-    background: Color.fromARGB(255, 226, 167, 40),
-    primary: Colors.grey.shade500,
+    background: Colors.cyan.shade400,
+    primary: Colors.yellow.shade800,
     secondary: Colors.grey.shade200 ,
     tertiary: Colors.white,
     inversePrimary: Colors.grey.shade900,
